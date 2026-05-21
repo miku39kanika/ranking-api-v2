4b76fc14601aea3a6d0ce92583ea7ab970444e44
miku39kanika <miku39kanika@gmail.com>
2026-05-21 19:02:53 +0900

    Kernel2

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -6,17 +6,20 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Console\Commands\GenerateGameQuestions;
 
-class Kernel extends ConsoleKernel
-{
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('game:generate')
-            ->everyMinute()->withoutOverlapping();
+// class Kernel extends ConsoleKernel
+// {
+//     protected function schedule(Schedule $schedule)
+//     {
+//         $schedule->command('game:generate')
+//             ->dailyAt('00:00')->withoutOverlapping();
 
-        $schedule->command('gift:create-login-bonus')
-            ->everyMinute()->withoutOverlapping();
+//         $schedule->command('gift:create-login-bonus')
+//             ->dailyAt('00:05')->withoutOverlapping();
 
-        $schedule->command('reward:daily-crowns')
-            ->everyMinute()->withoutOverlapping();
-    }
-};
+//         $schedule->command('reward:daily-crowns')
+//             ->dailyAt('00:10')->withoutOverlapping();
+
+//             $schedule->command('queue:work --stop-when-empty')
+//             ->everyMinute()->withoutOverlapping();
+//     }
+// };
--- backend/routes/console.php
+++ backend/routes/console.php
@@ -3,13 +3,17 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('game:generate')
-    ->everyMinute()
+    ->dailyAt('00:00')
     ->withoutOverlapping();
 
 Schedule::command('gift:create-login-bonus')
-    ->everyMinute()
+    ->dailyAt('00:00')
     ->withoutOverlapping();
 
 Schedule::command('reward:daily-crowns')
+    ->dailyAt('00:00')
+    ->withoutOverlapping();
+
+Schedule::command('queue:work --stop-when-empty')
     ->everyMinute()
     ->withoutOverlapping();
