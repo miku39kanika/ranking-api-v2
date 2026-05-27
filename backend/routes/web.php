91d4a55822ed03552859fc298e1f21e610d7a46d
miku39kanika <miku39kanika@gmail.com>
2026-05-27 15:28:15 +0900

    kiyaku_2

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    Route::view('/terms', 'terms');
-
-    Route::view('/privacy', 'privacy');
+    return view('welcome');
 });
+
+Route::view('/terms', 'terms');
+
+Route::view('/privacy', 'privacy');
